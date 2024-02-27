6512e705a11a34535cc0c91e59cff3fc6c6f7bed
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-02-27 21:58:27 +0700

    up forgotpasswork

--- src/pages/ForgotPW/index.js
+++ src/pages/ForgotPW/index.js
@@ -1,48 +1,69 @@
-// import { ErrorMessage, Field, Formik } from 'formik';
-// import React, { useEffect, useState } from 'react';
-// import { Form, useNavigate } from 'react-router-dom';
-// import * as Yup from 'yup';
-// function ForgotPassword() {
-//     return (
-//         <>
-//             <Formik
-//                 initialValues={{ email: '' }}
-//                 validationSchema={Yup.object({
-//                     email: Yup.string().email('Email không hợp lệ').required('Bắt buộc nhập email'),
-//                 })}
-//             >
-//                 <div className="bg-gray-50 dark:bg-gray-900">
-//                     <div className="flex flex-col items-center justify-center px-6 py-8">
-//                         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-//                             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-//                                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-//                                     QUÊN MẬT KHẨU
-//                                 </h1>
-//                                 <Form className="space-y-4 md:space-y-6" action="#">
-//                                     <div>
-//                                         <label htmlFor="email" className="text-gray-700 dark:text-gray-300">
-//                                             Email
-//                                         </label>
-//                                         <Field
-//                                             name="email"
-//                                             type="email"
-//                                             className="w-full form-input dark:border-gray-600 dark:bg-gray-700"
-//                                         />
-//                                         <ErrorMessage name="email" component="div" className="text-red-500" />
-//                                     </div>
-//                                     <div>
-//                                         <button type="submit" className="w-full btn btn-primary">
-//                                             Gửi yêu cầu
-//                                         </button>
-//                                     </div>
-//                                 </Form>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </Formik>
-//         </>
-//     );
-// }
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import React, { useState } from 'react';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../config';
+import { useNavigate } from 'react-router-dom';
 
-// export default ForgotPassword;
+function ForgotPassword() {
+    const [email, setEmail] = useState('');
+    const history = useNavigate();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await sendPasswordResetEmail(auth, email);
+            alert('Đã gửi yêu cầu đổi mật khẩu!');
+            history('/sign-up');
+        } catch (error) {
+            alert('Email không tồn tại!');
+        }
+    };
+    return (
+        <div>
+            <Formik
+                initialValues={{ email: '' }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert('Đã gửi yêu cầu đổi mật khẩu!');
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                <div className="bg-gray-50 dark:bg-gray-900">
+                    <div className="flex flex-col items-center justify-center px-6 py-8">
+                        <Form className="w-full max-w-sm">
+                            <div className="mb-6">
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"
+                                >
+                                    Email
+                                </label>
+                                <Field
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                    placeholder="Nhập email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <ErrorMessage name="email" component="div" className="text-red-500" />
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <button
+                                    type="submit"
+                                    className="w-full flex justify-center bg-green-900 border border-transparent rounded-md py-2 px-4 text-sm font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+                                    onClick={handleSubmit}
+                                >
+                                    Gửi yêu cầu
+                                </button>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </Formik>
+        </div>
+    );
+}
+
+export default ForgotPassword;
